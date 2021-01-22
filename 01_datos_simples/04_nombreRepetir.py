926bb1b69fe80c2704dcb8e54707b6877b7e37fa
Julio <77311954+juliocdm@users.noreply.github.com>
2021-01-21 20:22:45 -0600

    Archivos agregados vía web
    Primeros archivos de ejercicios de python con dificultad progresiva.

---
+++ 01_datos_simples/04_nombreRepetir.py
@@ -0,0 +1,4 @@
+nombre = str(input("Escribe tu nombre: "))
+nombre = nombre + " "
+repeticiones = int(input("Escribe el número de veces a repetir el nombre: "))
+print(nombre * repeticiones)
---
+++ 01_datos_simples/05_nombreMayusLong.py
@@ -0,0 +1,3 @@
+nombre = str(input("Escribe tu nombre: "))
+nombre = nombre.upper()
+print(nombre, "tiene " + str(len(nombre)) + " letras")
